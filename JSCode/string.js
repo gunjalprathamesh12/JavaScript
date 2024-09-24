c2ab04e91714b0e14f7dd899dbe975561aa11416
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2024-09-24 16:35:45 +0530

    String js

--- JSCode/string.js
+++ JSCode/string.js
@@ -2,4 +2,7 @@ var a = "PRATHAMESH";
 console.log(a.length());
 console.log(a.charCodeAt(0));
 console.log(a.charCodeAt(0));
-console.log(a.slice(2,3));
+console.log(a.slice(2, 3));
+
+var slicedPart = a.slice(2, 6);
+console.log(slicedPart);
