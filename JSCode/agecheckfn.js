1ec368e20acac7a789aa11402946596116479b93
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2024-09-27 20:03:02 +0530

    agecheck function

---
+++ JSCode/agecheckfn.js
@@ -0,0 +1,11 @@
+console.log("Hey user pls enter your age pls ");
+var age = prompt("age=");
+age = parseInt(age);
+
+function agecheck() {
+  if (age < 18) {
+    console.log("You are not Adult");
+  } else {
+    console.log("You are adult");
+  }
+}
