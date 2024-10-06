bf53f8e39ba05e7da95ed7385c4389a6a2272c67
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2024-10-06 10:16:58 +0530

    Methods Of Array

---
+++ JSCode/unshift.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);
+
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Kela");
+console.log(fruit);
