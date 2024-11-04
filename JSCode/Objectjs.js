2e6dec34469cb7dfc202d963ed5612909c522988
gunjalprathamesh12 <gunjalprathamesh1212@gmail.com>
2024-11-04 06:30:21 +0530

    js

--- JSCode/Objectjs.js
+++ JSCode/Objectjs.js
@@ -1,5 +1,16 @@
+//obj create way 1
 var person = {
   name: "Ravi",
   age: 20,
   proffesion: "developer",
 };
+console.log(person.name);
+console.log(person.age);
+console.log(person.proffesion);
+
+// way 2
+const car = new Object();//
+car.name = "bmw";
+car.color = "balck";
+console.log(car.name);
+console.log(car.color);
